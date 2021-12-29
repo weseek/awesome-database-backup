2a5de691f35cf3e585219bf8f2052c8ddcc008e8
Ryu Sato <ryu@weseek.co.jp>
2021-12-29 15:25:34 +0900

    enable backup to execute

--- packages/mongodb/bin/backup.ts
+++ packages/mongodb/bin/backup.ts
@@ -1,31 +1,49 @@
 #!/usr/bin/env node
 
 /**
- * USAGE:
- *   node bin/backup   # backup
+ * Usage: backup [options] <TARGET_BUCKET_URL>
+ *
+ * Arguments:
+ *   TARGET_BUCKET_URL                                URL of target bucket
+ *
+ * Options:
+ *   -V, --version                                    output the version number
+ *   --aws-access-key-id <AWS_ACCESS_KEY_ID>          Your IAM Access Key ID
+ *   --aws-secret-access-key <AWS_SECRET_ACCESS_KEY>  Your IAM Secret Access Key
+ *   --backup-file-prefix                             Prefix of backup file. (default: "backup")
+ *   --mongodb-host                                   Specifies the resolvable hostname of the MongoDB deployment. By default, `backup` attempts to connect to a MongoDB instance running on the "mongo" on port number 27017. (default: "mongo")
+ *   --cronmode                                       Run `backup` as cron mode. In Cron mode, `backup` will be executed periodically. (default: false)
+ *   -h, --help                                       display help for command
  */
 
+import { program } from 'commander';
+import { basename } from 'path';
 import { generateProvider } from '../../core/src';
-import { readFileSync } from 'fs';
-import { program, Option } from 'commander';
 
-const version = JSON.parse(readFileSync('../package.json').toString()).version;
+const packageJson = require('../package.json');
+const version = packageJson.version;
 
 program
   .version(version)
-  .option('--backup-file-prefix', 'Prefix of backup file.', 'backup')
+  .argument('<TARGET_BUCKET_URL>', 'URL of target bucket')
+  // Required fields that are intentionally treat as optional so that they can be specified by environment variables.
+  .requiredOption('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Your IAM Access Key ID', process.env['AWS_ACCESS_KEY_ID'])
+  .requiredOption('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Your IAM Secret Access Key', process.env['AWS_SECRET_ACCESS_KEY'])
+  .option('--backupfile-prefix', 'Prefix of backup file.', 'backup')
   .option('--mongodb-host', 'Specifies the resolvable hostname of the MongoDB deployment. By default, `backup` attempts to connect to a MongoDB instance running on the "mongo" on port number 27017.', 'mongo')
   .option('--cronmode', 'Run `backup` as cron mode. In Cron mode, `backup` will be executed periodically.', false)
-  .addOption(new Option('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Your IAM Access Key ID').env('AWS_ACCESS_KEY_ID'))
-  .addOption(new Option('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Your IAM Secret Access Key').env('AWS_SECRET_ACCESS_KEY'))
-  .argument('<TARGET_BUCKET_URL>', 'URL of target bucket')
-
-program
-  .parse(process.argv)
   .action((targetBucketUrl) => {
-    try {
-      generateProvider(targetBucketUrl);
-    } catch(e) {
-      throw new Error(`Cannot generate factory: ${e}`);
-    }
+    console.log(`=== ${basename(__filename)} started at ${Date().toLocaleString()} ===`);
+    const provider = generateProvider(targetBucketUrl);
+    provider
+      .listFiles(targetBucketUrl)
+      .then((listFiles) => {
+        console.log(`--- files of ${targetBucketUrl} ---`);
+        console.log(listFiles);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   })
+
+program.parse(process.argv)
