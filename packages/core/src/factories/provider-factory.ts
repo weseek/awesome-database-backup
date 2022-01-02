a12cab2aa84f2da6813242ec87c735d1bb58f50b
Ryu Sato <ryu@weseek.co.jp>
2022-01-03 00:44:18 +0900

    add tsconfig-path

--- packages/core/src/factories/provider-factory.ts
+++ packages/core/src/factories/provider-factory.ts
@@ -1,12 +1,13 @@
 import { IProvider } from '@awesome-backup/core/interfaces/provider';
 import { S3Provider } from '@awesome-backup/core/providers/s3';
 import { GCSProvider } from '@awesome-backup/core/providers/gcs';
+import { S3ClientConfig } from '@aws-sdk/client-s3';
 
-export function generateProvider(url: string): IProvider {
+export function generateProvider(url: string, config: S3ClientConfig|null = null): IProvider {
   const urlObj = new URL(url);
   switch (urlObj.protocol) {
     case 's3:':
-      return new S3Provider({});
+      return new S3Provider(config || {});
     case 'gcs:':
       return new GCSProvider();
   }
--- packages/mongodb/bin/backup.ts
+++ packages/mongodb/bin/backup.ts
@@ -11,26 +11,30 @@
  *   --aws-access-key-id <AWS_ACCESS_KEY_ID>          Your IAM Access Key ID
  *   --aws-secret-access-key <AWS_SECRET_ACCESS_KEY>  Your IAM Secret Access Key
  *   --backup-file-prefix                             Prefix of backup file. (default: "backup")
- *   --mongodb-host                                   Specifies the resolvable hostname of the MongoDB deployment. By default, `backup` attempts to connect to a MongoDB instance running on the "mongo" on port number 27017. (default: "mongo")
+ *   --mongodb-host                                   Specifies the resolvable hostname of the MongoDB deployment.
+ *                                                    By default, `backup` attempts to connect to a MongoDB instance
+ *                                                    running on the "mongo" on port number 27017. (default: "mongo")
  *   --cronmode                                       Run `backup` as cron mode. In Cron mode, `backup` will be executed periodically. (default: false)
  *   -h, --help                                       display help for command
  */
 
 import { program } from 'commander';
-import { basename } from 'path';
+import { basename, join } from 'path';
 import { generateProvider } from '@awesome-backup/core';
 
-const packageJson = require('../package.json');
+const packageJson = require(join(__dirname, '..', 'package.json'));
 const version = packageJson.version;
 
 program
   .version(version)
   .argument('<TARGET_BUCKET_URL>', 'URL of target bucket')
   // Required fields that are intentionally treat as optional so that they can be specified by environment variables.
-  .requiredOption('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Your IAM Access Key ID', process.env['AWS_ACCESS_KEY_ID'])
-  .requiredOption('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Your IAM Secret Access Key', process.env['AWS_SECRET_ACCESS_KEY'])
+  .requiredOption('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Your IAM Access Key ID', process.env.AWS_ACCESS_KEY_ID)
+  .requiredOption('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Your IAM Secret Access Key', process.env.AWS_SECRET_ACCESS_KEY)
   .option('--backupfile-prefix', 'Prefix of backup file.', 'backup')
-  .option('--mongodb-host', 'Specifies the resolvable hostname of the MongoDB deployment. By default, `backup` attempts to connect to a MongoDB instance running on the "mongo" on port number 27017.', 'mongo')
+  .option('--mongodb-host', 'Specifies the resolvable hostname of the MongoDB deployment.'
+          + 'By default, `backup` attempts to connect to a MongoDB instance'
+          + 'running on the "mongo" on port number 27017.', 'mongo')
   .option('--cronmode', 'Run `backup` as cron mode. In Cron mode, `backup` will be executed periodically.', false)
   .action((targetBucketUrl) => {
     console.log(`=== ${basename(__filename)} started at ${Date().toLocaleString()} ===`);
@@ -41,9 +45,9 @@ program
         console.log(`--- files of ${targetBucketUrl} ---`);
         console.log(listFiles);
       })
-      .catch((e) => {
+      .catch((e: any) => {
         console.log(e);
       });
   })
 
-program.parse(process.argv)
+program.parse(process.argv);
