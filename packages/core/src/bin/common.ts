58d229c7d82c8b87a73850b2e3318b30b40a18dc
Ryu Sato <ryu@weseek.co.jp>
2022-01-23 15:18:55 +0900

    refactor

--- packages/core/src/bin/common.ts
+++ packages/core/src/bin/common.ts
@@ -1,11 +1,6 @@
 import { Command } from 'commander';
-import {
-  configExistS3, createConfigS3,
-} from '../factories/provider-config-factory';
 import { IProvider } from '../interfaces/provider';
-import { getProviderType } from '../factories/provider-factory';
-import { S3Provider } from '../providers/s3';
-import { GCSProvider } from '../providers/gcs';
+import { getProviderType, generateS3Provider, generateGCSProvider } from '../factories/provider-factory';
 
 /* List command option types */
 export declare interface ICommonCLIOption {
@@ -46,14 +41,12 @@ export class BinCommon extends Command {
 
         switch (type) {
           case 'S3':
-            this.validateS3ProviderOption(options);
-            this.provider = this.s3Provider(options);
-            break;
+            this.provider = generateS3Provider(options);
+            return;
 
           case 'GCS':
-            this.validateGCSProviderOption(options);
-            this.provider = this.gcsProvider(options);
-            break;
+            this.provider = generateGCSProvider(options);
+            return;
         }
       });
   }
@@ -74,57 +67,4 @@ export class BinCommon extends Command {
         'JSON file path to your GCP Service Account Key', process.env.GOOGLE_APPLICATION_CREDENTIALS);
   }
 
-  private validateS3ProviderOption(options: ICommonCLIOption): void {
-
-    if (configExistS3()) return;
-
-    if (!options.awsRegion || !options.awsAccessKeyId || !options.awsSecretAccessKey) {
-      throw new Error('If the configuration file does not exist, '
-                        + 'you will need to set "--aws-region", "--aws-access-key-id", and "--aws-secret-access-key".');
-    }
-  }
-
-  private validateGCSProviderOption(options: ICommonCLIOption): void {
-
-    if (!options.gcpProjectId) {
-      throw new Error('You will need to set "--gcp-project-id".');
-    }
-    if (!(options.gcpServiceAccountKeyJsonPath || (options.gcpClientEmail && options.gcpPrivateKey))) {
-      throw new Error('You will need to set both "--gcp-access-key-id" and "--gcp-secret-access-key", or "--gcp-service-account-key-json-path".');
-    }
-  }
-
-  /* If the configuration file does not exist, it is created temporarily from the options,
-    and it will be deleted when process exit. */
-  private s3Provider({
-    awsRegion,
-    awsAccessKeyId,
-    awsSecretAccessKey,
-  }: ICommonCLIOption): S3Provider|null {
-    if (awsRegion == null || awsAccessKeyId == null || awsSecretAccessKey == null) return null;
-
-    createConfigS3({ awsRegion, awsAccessKeyId, awsSecretAccessKey });
-    return new S3Provider({});
-  }
-
-  /* Configuration file is created temporarily from the options, and it will be deleted when process exit. */
-  private gcsProvider({
-    gcpServiceAccountKeyJsonPath,
-    gcpProjectId,
-    gcpClientEmail,
-    gcpPrivateKey,
-  }: ICommonCLIOption): GCSProvider|null {
-    if (gcpServiceAccountKeyJsonPath) {
-      return new GCSProvider({ keyFilename: gcpServiceAccountKeyJsonPath });
-    }
-
-    if (gcpPrivateKey == null) return null;
-    return new GCSProvider({
-      projectId: gcpProjectId,
-      credentials: {
-        client_email: gcpClientEmail,
-        private_key: gcpPrivateKey.replace(/\\n/g, '\n'),
-      },
-    });
-  }
 }
--- packages/core/src/factories/provider-factory.ts
+++ packages/core/src/factories/provider-factory.ts
@@ -1,4 +1,9 @@
 import { ProviderType } from '../interfaces/provider';
+import {
+  configExistS3, createConfigS3,
+} from './provider-config-factory';
+import { S3Provider } from '../providers/s3';
+import { GCSProvider } from '../providers/gcs';
 
 export function getProviderType(target: URL): ProviderType|undefined {
   const typeMap: Record<string, ProviderType> = {
@@ -7,4 +12,59 @@ export function getProviderType(target: URL): ProviderType|undefined {
   };
   const key = target.protocol.replace(/:/, '');
   return typeMap[key];
+}
+
+export function generateS3Provider({
+  awsRegion,
+  awsAccessKeyId,
+  awsSecretAccessKey,
+}: {
+  awsRegion?: string,
+  awsAccessKeyId?: string,
+  awsSecretAccessKey?: string,
+}): S3Provider {
+  /* If the configuration file does not exist, it is created temporarily from the options,
+    and it will be deleted when process exit. */
+  if (!configExistS3()) {
+    if (!awsRegion || !awsAccessKeyId || !awsSecretAccessKey) {
+      throw new Error('If the configuration file does not exist, '
+                        + 'you will need to set "--aws-region", "--aws-access-key-id", and "--aws-secret-access-key".');
+    }
+    createConfigS3({ awsRegion, awsAccessKeyId, awsSecretAccessKey });
+  }
+
+  return new S3Provider({});
+}
+
+export function generateGCSProvider({
+  gcpServiceAccountKeyJsonPath,
+  gcpProjectId,
+  gcpClientEmail,
+  gcpPrivateKey,
+}: {
+  gcpServiceAccountKeyJsonPath?: string,
+  gcpProjectId?: string,
+  gcpClientEmail?: string,
+  gcpPrivateKey?: string,
+}): GCSProvider {
+  if (gcpServiceAccountKeyJsonPath) {
+    return new GCSProvider({ keyFilename: gcpServiceAccountKeyJsonPath });
+  }
+
+  /* If the configuration file does not exist, it is created temporarily from the options,
+    and it will be deleted when process exit. */
+  if (![gcpProjectId, gcpClientEmail, gcpPrivateKey].every(it => it != null)) {
+    throw new Error('If you does not set "--gcp-service-account-key-json-path", '
+                      + 'you will need to set all of "--gcp-project-id", "--gcp-access-key-id" and "--gcp-secret-access-key".');
+  }
+
+  // [MEMO] Converting escaped characters because newline codes cannot be entered in the commander argument.
+  const privateKey = gcpPrivateKey?.replace(/\\n/g, '\n');
+  return new GCSProvider({
+    projectId: gcpProjectId,
+    credentials: {
+      client_email: gcpClientEmail,
+      private_key: privateKey,
+    },
+  });
 }
