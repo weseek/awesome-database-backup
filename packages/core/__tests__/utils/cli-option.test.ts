da8e2b51af0f8271e93b5e6955eb81a821cbd0e9
Ryu Sato <ryu@weseek.co.jp>
2022-01-25 00:35:13 +0900

    adjust test

--- packages/core/__tests__/utils/cli-option.test.ts
+++ packages/core/__tests__/utils/cli-option.test.ts
@@ -26,74 +26,21 @@ describe('#execute', () => {
   describe('when called with only "command"', () => {
     it('call "exec" with command', async() => {
       await cliOption.execute('command_name');
-      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name', expect.any(Function));
+      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name  ', expect.any(Function));
     });
   });
 
   describe('when called with arguments', () => {
     it('call "exec" with command and arguments separated by space', async() => {
-      await cliOption.execute('command_name', ['arg1']);
-      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name arg1', expect.any(Function));
+      await cliOption.execute('command_name', 'arg1');
+      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name  arg1', expect.any(Function));
     });
   });
 
   describe('when called with option', () => {
     it('call "exec" with command and option separated by space', async() => {
-      await cliOption.execute('command_name', [], { '--opt1': 'value1' });
-      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 value1', expect.any(Function));
-    });
-  });
-
-  describe('when called with option and default option', () => {
-    describe('and when same option is specified', () => {
-      it('call "exec" with command and option which override default option', async() => {
-        await cliOption.execute('command_name', [], { '--opt1': 'value1' }, { '--opt1': 'defaultValue1' });
-        expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 value1', expect.any(Function));
-      });
-    });
-
-    describe('and when only default option is specified', () => {
-      it('call "exec" with command and option which override default option', async() => {
-        await cliOption.execute('command_name', [], {}, { '--opt1': 'defaultValue1' });
-        expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 defaultValue1', expect.any(Function));
-      });
-    });
-  });
-
-  describe('when called with option which include boolean value', () => {
-    it('call "exec" with command and option without value', async() => {
-      await cliOption.execute('command_name', [], { '--opt1': true });
-      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1', expect.any(Function));
-    });
-  });
-
-  describe('when called with option which include number value', () => {
-    it('call "exec" with command and option with number', async() => {
-      await cliOption.execute('command_name', [], { '--opt1': 1 });
-      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 1', expect.any(Function));
-    });
-  });
-
-  describe('when called with option which include array value', () => {
-    describe('and when no default option is specified', () => {
-      it('call "exec" with a space-separated command and list of options', async() => {
-        await cliOption.execute('command_name', [], { '--opt1': ['value1', 'value2'] });
-        expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 value1 value2', expect.any(Function));
-      });
-    });
-
-    describe('and when default option is specified', () => {
-      it('call "exec" with a space-separated command and list of options that replace default values', async() => {
-        await cliOption.execute('command_name', [], { '--opt1': ['value1', 'value2'] }, { '--opt1': ['defaultValue1'] });
-        expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 value1 value2', expect.any(Function));
-      });
-    });
-
-    describe('and when set "listValueEnabled" is false', () => {
-      it('call "exec" with a space-separated command and a repeated list of options', async() => {
-        await cliOption.execute('command_name', [], { '--opt1': ['value1', 'value2'] }, {}, false);
-        expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 value1 --opt1 value2', expect.any(Function));
-      });
+      await cliOption.execute('command_name', '', '--opt1 value1');
+      expect(childProcessModuleMock.exec).toHaveBeenCalledWith('command_name --opt1 value1 ', expect.any(Function));
     });
   });
 
--- packages/core/src/utils/cli-option.ts
+++ packages/core/src/utils/cli-option.ts
@@ -1,50 +1,6 @@
 
 import { exec } from 'child_process';
 
-const kebabCase = require('kebab-case');
-
-function _camelize(str: string) {
-  return (str.charAt(0).toLowerCase() + str.slice(1)).replace(/[-_](.)/g, (match, firstLetterOfWord) => firstLetterOfWord.toUpperCase());
-}
-
-export function convertOption(
-    option: Record<string, string|number|boolean|string[]|number[]>,
-    filter: (key: string) => boolean,
-    converter: (key: string) => string,
-): Record<string, string|number|boolean|string[]|number[]> {
-  const convertedOption: Record<string, string|number|boolean|string[]|number[]> = {};
-
-  Object.keys(option)
-    .filter(key => filter(key))
-    .forEach((key) => {
-      const convertedKey = converter(key);
-      convertedOption[convertedKey] = Array.isArray(option[key]) ? option[key] : option[key].toString();
-    });
-  return convertedOption;
-}
-
-export function convertOptionAsKebabCase(
-    option: Record<string, string|number|boolean|string[]|number[]>,
-    prefix: string,
-): Record<string, string|number|boolean|string[]|number[]> {
-  return convertOption(
-    option,
-    (key: string) => kebabCase(key.startsWith(`${prefix}-`)),
-    (key: string) => `--${kebabCase(key).replace(new RegExp(`${prefix}-`, 'g'), '')}`,
-  );
-}
-
-export function convertOptionAsCamelCase(
-    option: Record<string, string|number|boolean|string[]|number[]>,
-    prefix: string,
-): Record<string, string|number|boolean|string[]|number[]> {
-  return convertOption(
-    option,
-    (key: string) => kebabCase(key).startsWith(`${prefix}-`),
-    (key: string) => `--${_camelize(key.replace(new RegExp(`${prefix}`, 'g'), ''))}`,
-  );
-}
-
 export async function execute(
     command: string,
     args?: string,
