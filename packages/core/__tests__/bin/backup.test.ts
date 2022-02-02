3fd8c415182ee6c480a25bbc84ecf7b8ea73661a
Ryu Sato <ryu@weseek.co.jp>
2022-02-03 00:04:59 +0900

    add test

--- packages/core/__tests__/bin/backup.test.ts
+++ packages/core/__tests__/bin/backup.test.ts
@@ -4,20 +4,23 @@ let backup = require('../../src/bin/backup');
 
 describe('BackupCommand', () => {
   describe('backupOnce', () => {
-    describe('when healthchecksUrl is empty', () => {
-      beforeEach(() => {
-        jest.resetModules();
-        jest.doMock('../../src/utils/tar', () => {
-          const actual = jest.requireActual('../../src/utils/tar');
-          return {
-            ...actual,
-            compress: jest.fn().mockReturnValue(''),
-          };
-        });
-        backup = require('../../src/bin/backup');
+    beforeEach(() => {
+      jest.resetModules();
+      jest.doMock('../../src/utils/tar', () => {
+        const actual = jest.requireActual('../../src/utils/tar');
+        return {
+          ...actual,
+          compress: jest.fn().mockReturnValue(''),
+        };
       });
+      backup = require('../../src/bin/backup');
+    });
+    afterEach(() => {
+      jest.dontMock('../../src/utils/tar');
+    });
 
-      it('return undefined', async() => {
+    describe('when healthchecksUrl is empty', () => {
+      it('return undefined and call dumpDatabaseFunc()', async() => {
         const bakupCommand = new backup.BackupCommand();
         const storageServiceClientMock = {
           copyFile: jest.fn()
@@ -28,6 +31,41 @@ describe('BackupCommand', () => {
           backupfilePrefix: 'backup',
         };
         await expect(bakupCommand.backupOnce(storageServiceClientMock, dumpDatabaseFuncMock, targetBucketUrl, options)).resolves.toBe(undefined);
+        expect(dumpDatabaseFuncMock).toBeCalled();
+      });
+    });
+
+    describe('when healthcheckUrl is not empty', () => {
+      const axiosGetMock = jest.fn().mockReturnValue(Promise.resolve());
+
+      beforeEach(() => {
+        jest.resetModules();
+        jest.doMock('axios', () => {
+          const actual = jest.requireActual('axios');
+          return {
+            ...actual,
+            get: axiosGetMock,
+          };
+        });
+        backup = require('../../src/bin/backup');
+      });
+      afterEach(() => {
+        jest.dontMock('axios');
+      });
+
+      it('return undefined and call axios with "healthcheckUrl"', async() => {
+        const bakupCommand = new backup.BackupCommand();
+        const storageServiceClientMock = {
+          copyFile: jest.fn()
+        };
+        const dumpDatabaseFuncMock = jest.fn().mockReturnValue({ stdout: "", stderr: "" });
+        const targetBucketUrl = new URL('gs://sample.com/bucket');
+        const options: IBackupCLIOption = {
+          backupfilePrefix: 'backup',
+          healthchecksUrl: 'http://example.com/',
+        };
+        await expect(bakupCommand.backupOnce(storageServiceClientMock, dumpDatabaseFuncMock, targetBucketUrl, options)).resolves.toBe(undefined);
+        expect(axiosGetMock).toBeCalled();
       });
     });
   });
