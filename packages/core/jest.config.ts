0282dfba77d46507584146d6c4265e8c94d0e18f
Ryu Sato <ryu@weseek.co.jp>
2022-02-06 16:42:03 +0900

    debug

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -189,6 +189,7 @@ export default {
 
   // Indicates whether each individual test should be reported during the run
   // verbose: undefined,
+  verbose: true,
 
   // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
   // watchPathIgnorePatterns: [],
