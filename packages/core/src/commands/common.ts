1d4081696f58c5fdcbc60d4700f41e156d70c535
Ryu Sato <ryu@weseek.co.jp>
2022-03-22 03:41:20 +0900

    use env method instead of settings env as default value

--- packages/core/src/commands/common.ts
+++ packages/core/src/commands/common.ts
@@ -2,7 +2,7 @@
  * Define common processing for "backup", "restore", "list", and "prune" commands.
  */
 
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 import { IStorageServiceClient } from '../interfaces/storage-service-client';
 import { storageProviderType, generateStorageServiceClient } from '../factories/provider-factory';
 
@@ -21,14 +21,14 @@ export function addStorageServiceClientOptions(command: Command): void {
   command
     /* AWS options */
     .option('--aws-region <AWS_REGION>', 'AWS Region')
-    .option('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Your IAM Access Key ID', process.env.AWS_ACCESS_KEY_ID)
-    .option('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Your IAM Secret Access Key', process.env.AWS_SECRET_ACCESS_KEY)
+    .addOption(new Option('--aws-access-key-id <AWS_ACCESS_KEY_ID>', 'Your IAM Access Key ID').env('AWS_ACCESS_KEY_ID'))
+    .addOption(new Option('--aws-secret-access-key <AWS_SECRET_ACCESS_KEY>', 'Your IAM Secret Access Key').env('AWS_SECRET_ACCESS_KEY'))
     /* GCS options */
-    .option('--gcp-project-id <GCP_PROJECT_ID>', 'GCP Project ID', process.env.GCLOUD_PROJECT)
+    .addOption(new Option('--gcp-project-id <GCP_PROJECT_ID>', 'GCP Project ID').env('GCLOUD_PROJECT'))
     .option('--gcp-private-key <GCP_PRIVATE_KEY>', 'GCP Private Key')
-    .option('--gcp-client-email, <GCP_CLIENT_EMAIL>', 'GCP Client Email')
-    .option('--gcp-service-account-key-json-path <GCP_SERVICE_ACCOUNT_KEY_JSON_PATH>',
-      'JSON file path to your GCP Service Account Key', process.env.GOOGLE_APPLICATION_CREDENTIALS);
+    .option('--gcp-client-email <GCP_CLIENT_EMAIL>', 'GCP Client Email')
+    .addOption(new Option('--gcp-service-account-key-json-path <GCP_SERVICE_ACCOUNT_KEY_JSON_PATH>',
+      'JSON file path to your GCP Service Account Key').env('GOOGLE_APPLICATION_CREDENTIALS'));
 }
 
 export function addStorageServiceClientGenerateHook(command: Command, storageServiceClientHolder: { storageServiceClient: IStorageServiceClient|null }): void {
