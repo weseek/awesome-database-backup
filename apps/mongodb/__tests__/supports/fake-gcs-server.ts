0ded9c22afbbefd12724153621dda5f297611372
Ryu Sato <ryu@weseek.co.jp>
2022-04-02 16:56:04 +0900

    add supports of fake-gcs-server for jest

---
+++ apps/mongodb/__tests__/supports/fake-gcs-server.ts
@@ -0,0 +1,26 @@
+import { Storage } from '@google-cloud/storage';
+import { fixturePath } from '../fixtures';
+
+const storage = new Storage({
+  projectId: 'valid_project_id',
+  credentials: {
+    client_email: 'valid@example.com',
+    private_key: 'valid_private_key',
+  },
+  apiEndpoint: 'http://fake-gcs-server:4443',
+});
+
+export const testGCSBucketName = 'test';
+export const testGCSBucketURI = `gs://${testGCSBucketName}`;
+export async function cleanTestGCSBucket(): Promise<void> {
+  await storage.createBucket(testGCSBucketName);
+  await storage.bucket(testGCSBucketName).deleteFiles({ force: true });
+  await storage.bucket(testGCSBucketName).delete({ ignoreNotFound: true });
+  await storage.createBucket(testGCSBucketName);
+}
+
+export async function uploadFixtureToTestBucket(fixtureName: string): Promise<void> {
+  await storage.bucket(testGCSBucketName).upload(fixturePath(fixtureName), {
+    destination: fixtureName,
+  });
+}
