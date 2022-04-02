72cc773373b37279044a7b764ef4319458a4d090
Ryu Sato <ryu@weseek.co.jp>
2022-04-02 22:32:52 +0900

    add tests

---
+++ apps/postgresql/__tests__/bin/backup.test.ts
@@ -0,0 +1,85 @@
+import { exec as execOriginal } from 'child_process';
+import { promisify } from 'util';
+import {
+  testS3BucketURI,
+  cleanTestS3Bucket,
+} from '../supports/s3rver';
+import {
+  testGCSBucketURI,
+  cleanTestGCSBucket,
+  listFileNamesInTestGCSBucket,
+} from '../supports/fake-gcs-server';
+import { prepareTestPG } from '../supports/postgresql';
+
+const exec = promisify(execOriginal);
+
+const execBackupCommand = 'yarn run ts-node src/bin/backup';
+
+describe('backup', () => {
+  describe('when option --help is specified', () => {
+    const commandLine = `${execBackupCommand} --help`;
+    it('show help messages', async() => {
+      expect(await exec(commandLine)).toEqual({
+        stdout: expect.stringContaining('Usage:'),
+        stderr: '',
+      });
+    });
+  });
+
+  describe('when no option is specified', () => {
+    const commandLine = `${execBackupCommand}`;
+    it('throw error message', async() => {
+      await expect(exec(commandLine)).rejects.toThrowError(
+        /missing required argument 'TARGET_BUCKET_URL'/,
+      );
+    });
+  });
+
+  describe('when valid S3 options are specified', () => {
+    beforeEach(cleanTestS3Bucket);
+    beforeEach(prepareTestPG);
+
+    describe('and when backup tool options are specified', () => {
+      const commandLine = `PGPASSWORD="password" \
+        ${execBackupCommand} \
+        --aws-endpoint-url http://s3.s3rver \
+        --aws-region us-east-1 \
+        --aws-access-key-id "S3RVER" \
+        --aws-secret-access-key "S3RVER" \
+        --backup-tool-options "--host postgres --username postgres" \
+        ${testS3BucketURI}`;
+
+      it('backup PostgreSQL in bucket', async() => {
+        expect(await exec(commandLine)).toEqual({
+          stdout: expect.stringMatching(/=== backup.js started at .* ===/),
+          stderr: '',
+        });
+      });
+    });
+  });
+
+  describe('when valid GCS options are specified', () => {
+    beforeEach(cleanTestGCSBucket);
+    beforeEach(prepareTestPG);
+
+    describe('and when backup tool options are specified', () => {
+      const commandLine = `PGPASSWORD="password" \
+        ${execBackupCommand} \
+        --gcp-endpoint-url http://fake-gcs-server:4443 \
+        --gcp-project-id valid_project_id \
+        --gcp-client-email valid@example.com \
+        --gcp-private-key valid_private_key \
+        --backup-tool-options "--host postgres --username postgres" \
+        ${testGCSBucketURI}/`;
+
+      it('backup PostgreSQL in bucket', async() => {
+        expect((await listFileNamesInTestGCSBucket()).length).toBe(0);
+        expect(await exec(commandLine)).toEqual({
+          stdout: expect.stringMatching(/=== backup.js started at .* ===/),
+          stderr: '',
+        });
+        expect((await listFileNamesInTestGCSBucket()).length).toBe(1);
+      });
+    });
+  });
+});
---
+++ apps/postgresql/__tests__/bin/restore.test.ts
@@ -0,0 +1,95 @@
+import { exec as execOriginal } from 'child_process';
+import { promisify } from 'util';
+import {
+  cleanTestS3Bucket,
+  uploadFixtureToTestS3Bucket,
+} from '../supports/s3rver';
+import {
+  testGCSBucketURI,
+  cleanTestGCSBucket,
+  uploadFixtureToTestGCSBucket,
+} from '../supports/fake-gcs-server';
+import {
+  cleanTestPG,
+  listTableNamesInTestPG,
+} from '../supports/postgresql';
+
+const exec = promisify(execOriginal);
+
+const execRestoreCommand = 'yarn run ts-node src/bin/restore';
+
+describe('restore', () => {
+  describe('when option --help is specified', () => {
+    const commandLine = `${execRestoreCommand} --help`;
+    it('show help messages', async() => {
+      expect(await exec(commandLine)).toEqual({
+        stdout: expect.stringContaining('Usage:'),
+        stderr: '',
+      });
+    });
+  });
+
+  describe('when no option is specified', () => {
+    const commandLine = `${execRestoreCommand}`;
+    it('throw error message', async() => {
+      await expect(exec(commandLine)).rejects.toThrowError(
+        /missing required argument 'TARGET_BUCKET_URL'/,
+      );
+    });
+  });
+
+  describe('when valid S3 options are specified', () => {
+    const bucketURI = 's3://test/';
+    const objectURI = `${bucketURI}backup-20220402000000.tar.bz2`;
+    const commandLine = `PGPASSWORD="password" \
+      ${execRestoreCommand} \
+      --aws-endpoint-url http://s3.s3rver \
+      --aws-region us-east-1 \
+      --aws-access-key-id "S3RVER" \
+      --aws-secret-access-key "S3RVER" \
+      --restore-tool-options "--host postgres --username postgres" \
+      ${objectURI}`;
+
+    beforeEach(cleanTestS3Bucket);
+    beforeEach(cleanTestPG);
+    beforeEach(async() => {
+      await uploadFixtureToTestS3Bucket('backup-20220402000000.tar.bz2'); // includes 'dummy' table
+    });
+
+    it('restore PostgreSQL in bucket', async() => {
+      expect(await listTableNamesInTestPG()).toEqual([]);
+      expect(await exec(commandLine)).toEqual({
+        stdout: expect.stringMatching(/=== restore.js started at .* ===/),
+        stderr: '',
+      });
+      expect(await listTableNamesInTestPG()).toEqual(['dummy']);
+    });
+  });
+
+  describe('when valid GCS options are specified', () => {
+    const objectURI = `${testGCSBucketURI}/backup-20220402000000.tar.bz2`;
+    const commandLine = `PGPASSWORD="password" \
+      ${execRestoreCommand} \
+      --gcp-endpoint-url http://fake-gcs-server:4443 \
+      --gcp-project-id valid_project_id \
+      --gcp-client-email valid@example.com \
+      --gcp-private-key valid_private_key \
+      --restore-tool-options "--host postgres --username postgres" \
+      ${objectURI}`;
+
+    beforeEach(cleanTestGCSBucket);
+    beforeEach(cleanTestPG);
+    beforeEach(async() => {
+      await uploadFixtureToTestGCSBucket('backup-20220402000000.tar.bz2'); // includes 'dummy' table
+    });
+
+    it('restore PostgreSQL in bucket', async() => {
+      expect(await listTableNamesInTestPG()).toEqual([]);
+      expect(await exec(commandLine)).toEqual({
+        stdout: expect.stringMatching(/=== restore.js started at .* ===/),
+        stderr: '',
+      });
+      expect(await listTableNamesInTestPG()).toEqual(['dummy']);
+    });
+  });
+});
