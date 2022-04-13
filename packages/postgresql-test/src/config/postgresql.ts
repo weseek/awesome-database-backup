49cd2a498e588b7cf4137d0eefd3efe2b5a825ff
Ryu Sato <ryu@weseek.co.jp>
2022-04-14 01:02:42 +0900

    fix port type

--- packages/postgresql-test/src/config/postgresql.ts
+++ packages/postgresql-test/src/config/postgresql.ts
@@ -2,5 +2,5 @@ export const postgresqlConfig = {
   user: process.env.POSTGRESQL_USER || 'postgres',
   password: process.env.POSTGRESQL_PASSWORD || 'password',
   host: process.env.POSTGRESQL_HOST || 'postgres',
-  port: process.env.POSTGRESQL_PORT || 5432,
+  port: Number(process.env.POSTGRESQL_PORT) || 5432,
 };
