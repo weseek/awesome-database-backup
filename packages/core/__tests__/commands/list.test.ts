c2d22b3856ec1da41a429c455d4692285987af87
Ryu Sato <ryu@weseek.co.jp>
2022-04-18 22:57:04 +0900

    add test

--- packages/core/__tests__/commands/list.test.ts
+++ packages/core/__tests__/commands/list.test.ts
@@ -1,38 +1,125 @@
-const { ListCommand } = require('../../src/commands/list');
+import { ListCommand } from '../../src/commands/list';
 
 describe('ListCommand', () => {
-  describe('list', () => {
-    const options = {
-      targetBucketUrl: new URL('gs://sample.com/bucket'),
-    };
-    const storageServiceClientMock = {
-      listFiles: jest.fn().mockReturnValue(['']),
-    };
+  let command: ListCommand;
 
-    it('return undefined', async() => {
-      const listCommand = new ListCommand();
-      listCommand.storageServiceClient = storageServiceClientMock;
-      await expect(listCommand.list(options)).resolves.toBe(undefined);
+  // Default mock of logger
+  const loggerMock = {
+    info: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+  };
+  const storageServiceClientMock = {
+    name: '',
+    exists: jest.fn(),
+    listFiles: jest.fn().mockResolvedValue([]),
+    copyFile: jest.fn(),
+    deleteFile: jest.fn(),
+  };
+
+  beforeEach(() => {
+    jest.resetModules();
+
+    // Mock Logger
+    jest.doMock('universal-bunyan', () => {
+      return {
+        ...(jest.requireActual('universal-bunyan') as any),
+        createLogger: () => loggerMock,
+      };
+    });
+
+    const list = require('../../src/commands/list');
+    command = new list.ListCommand();
+    command.storageServiceClient = storageServiceClientMock;
+  });
+  afterEach(() => {
+    jest.dontMock('universal-bunyan');
+    jest.resetAllMocks();
+  });
+
+  describe('list', () => {
+
+    describe('when options are valid, but "storageServiceClient" is not set in advance', () => {
+      const options = {
+        targetBucketUrl: new URL('gs://sample.com/bucket'),
+      };
+
+      beforeEach(() => {
+        command.storageServiceClient = null;
+      });
+
+      it('reject with error', async() => {
+        await expect(command.list(options))
+          .rejects
+          .toThrowError('URL scheme is not that of a supported provider.');
+      });
+    });
+
+    describe('when options are valid and "storageServiceClient" is set in advance', () => {
+      beforeEach(() => {
+        command.storageServiceClient = storageServiceClientMock;
+      });
+
+      describe('and when storageServiceClient.listFiles() return empty', () => {
+        const options = {
+          targetBucketUrl: new URL('gs://sample.com/bucket'),
+        };
+
+        beforeEach(() => {
+          command.storageServiceClient = {
+            ...storageServiceClientMock,
+            listFiles: jest.fn().mockResolvedValue([]),
+          };
+        });
+
+        it('return undefined and not logging', async() => {
+          await expect(command.list(options)).resolves.toBe(undefined);
+          expect(loggerMock.info).toBeCalledTimes(0);
+        });
+      });
+
+      describe('and when storageServiceClient.listFiles() return file names', () => {
+        const options = {
+          targetBucketUrl: new URL('gs://sample.com/bucket'),
+        };
+
+        beforeEach(() => {
+          command.storageServiceClient = {
+            ...storageServiceClientMock,
+            listFiles: jest.fn().mockResolvedValue(['file1']),
+          };
+        });
+
+        it('return undefined and logging', async() => {
+          await expect(command.list(options)).resolves.toBe(undefined);
+          expect(loggerMock.info).toHaveBeenNthCalledWith(1, 'There are files below in bucket:');
+          expect(loggerMock.info).toHaveBeenNthCalledWith(2, 'file1');
+        });
+      });
     });
   });
 
   describe('addListOptions', () => {
+    beforeEach(() => {
+      jest.spyOn(command, 'addOption');
+    });
+
     it('call addOption()', () => {
-      const listCommand = new ListCommand();
-      const addOptionMock = jest.fn().mockReturnValue(listCommand);
-      listCommand.addOption = addOptionMock;
-      listCommand.addListOptions();
-      expect(addOptionMock).toBeCalled();
+      command.addListOptions();
+      expect(command.addOption).toBeCalled();
     });
   });
 
   describe('setListAction', () => {
+    beforeEach(() => {
+      jest.spyOn(command, 'saveStorageClientInAdvance');
+      jest.spyOn(command, 'action');
+    });
+
     it('call action()', () => {
-      const listCommand = new ListCommand();
-      const actionMock = jest.fn().mockReturnValue(listCommand);
-      listCommand.action = actionMock;
-      listCommand.setListAction();
-      expect(actionMock).toBeCalled();
+      command.setListAction();
+      expect(command.saveStorageClientInAdvance).toBeCalled();
+      expect(command.action).toBeCalled();
     });
   });
 });
