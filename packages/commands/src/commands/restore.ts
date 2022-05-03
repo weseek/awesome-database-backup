d3b74655d7f00f25de8384efe0a2be38053186b8
Ryu Sato <ryu@weseek.co.jp>
2022-05-04 04:29:13 +0900

    use some library instead of calling process "tar"

--- packages/commands/src/commands/restore.ts
+++ packages/commands/src/commands/restore.ts
@@ -1,14 +1,21 @@
 import { format } from 'date-fns';
-import { basename, join } from 'path';
+import {
+  basename, dirname, extname, join,
+} from 'path';
 import { Option } from 'commander';
 import { EOL } from 'os';
-import { expandBZIP2 } from '@awesome-backup/tar';
+import { createGunzip } from 'zlib';
+import { Transform } from 'stream';
+import * as StreamPromises from 'stream/promises';
+import { createReadStream } from 'fs';
 import { IRestoreCommandOption } from './interfaces';
 import { StorageServiceClientCommand } from './common';
 import loggerFactory from '../logger/factory';
 
 const logger = loggerFactory('mongodb-awesome-backup');
 const tmp = require('tmp');
+const tar = require('tar');
+const bz2 = require('unbzip2-stream');
 
 /**
  * Define actions, options, and arguments that are commonly required for restore command from the CLI, regardless of the database type.
@@ -25,6 +32,38 @@ export class RestoreCommand extends StorageServiceClientCommand {
     throw new Error('Method not implemented.');
   }
 
+  /**
+   * Extracts compressed file to the same directory.
+   *
+   * If the expantion succeeds, it returns the path to the expaneded file or directory.
+   * Extracted file or directory name is assumed same as compressed file name.
+   * (ex. Assume that file "some.tar.gz" is extracted to a directory "some")
+   */
+  async processBackupFile(backupFilePath: string): Promise<string> {
+    const processors: Record<string, Transform> = {
+      '.gz': createGunzip(),
+      '.bz2': bz2(),
+      '.tar': new tar.Unpack({ cwd: dirname(backupFilePath) }),
+    };
+
+    let newBackupFilePath = backupFilePath;
+    const streams = [];
+
+    streams.push(createReadStream(backupFilePath));
+    while (extname(newBackupFilePath) !== '') {
+      const ext = extname(newBackupFilePath);
+      if (processors[ext] == null) throw new Error(`Extension ${ext} is not supported`);
+
+      streams.push(processors[ext]);
+
+      newBackupFilePath = newBackupFilePath.slice(0, -ext.length);
+    }
+
+    return StreamPromises
+      .pipeline(streams)
+      .then(() => newBackupFilePath);
+  }
+
   async restore(options: IRestoreCommandOption): Promise<void> {
     if (this.storageServiceClient == null) throw new Error('URL scheme is not that of a supported provider.');
 
@@ -36,7 +75,7 @@ export class RestoreCommand extends StorageServiceClientCommand {
     await this.storageServiceClient.copyFile(options.targetBucketUrl.toString(), backupFilePath);
 
     logger.info(`expands ${backupFilePath}...`);
-    const { expandedPath } = await expandBZIP2(backupFilePath);
+    const expandedPath = await this.processBackupFile(backupFilePath);
     const { stdout, stderr } = await this.restoreDB(expandedPath, options.restoreToolOptions);
     if (stdout) stdout.split(EOL).forEach(line => logger.info(line));
     if (stderr) stderr.split(EOL).forEach(line => logger.warn(line));
