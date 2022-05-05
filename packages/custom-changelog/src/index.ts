682a3b5721f737943e0b18fbdd3893fc20296e36
Ryu Sato <ryu@weseek.co.jp>
2022-05-05 14:52:05 +0900

    ignore bot users for thanks

--- packages/custom-changelog/src/index.ts
+++ packages/custom-changelog/src/index.ts
@@ -1,7 +1,18 @@
+/* eslint-disable max-len */
 import { ChangelogFunctions } from '@changesets/types';
 import { getInfo, getInfoFromPullRequest } from '@changesets/get-github-info';
 
 const changelogFunctions: ChangelogFunctions = {
+
+  /*
+   * Return changelog's line for updated dependencies
+   *
+   * ex.
+   * - Updated dependencies [[`27a5a82`](https://github.com/changesets/changesets/commit/27a5a82188914570d192162f9d045dfd082a3c15)]:
+   *   - @changesets/types@4.1.0
+   *
+   * see. https://github.com/changesets/changesets/blob/main/packages/changelog-github/CHANGELOG.md
+   */
   getDependencyReleaseLine: async(
       changesets,
       dependenciesUpdated,
@@ -36,7 +47,36 @@ const changelogFunctions: ChangelogFunctions = {
 
     return [changesetLink, ...updatedDepenenciesList].join('\n');
   },
-  getReleaseLine: async(changeset, type, options) => {
+
+  /*
+   * Return changelog's line for Pull Request
+   *
+   * ex.
+   * - [#564](https://github.com/changesets/changesets/pull/564) [`707002d`](https://github.com/changesets/changesets/commit/707002dec9332a2c1322522a23861e747a6bff6f) Thanks [@Andarist](https://github.com/Andarist)! - It's now possible to specify multiple authors of a change by using multiple `author: @someuser` lines.
+   *
+   * see. https://github.com/changesets/changesets/blob/main/packages/changelog-github/CHANGELOG.md
+   *
+   * [TYPES]
+   *     type getReleaseLine(
+   *         changeset: {
+   *             // This is the string of the summary from the changeset markdown file
+   *             summary: string
+   *             // This is an array of information about what is going to be released. each is an object with name: the name of the package, and type, which is "major", "minor", or "patch"
+   *             releases
+   *             // the hash for the commit that introduced the changeset
+   *             commit
+   *         },
+   *         // the type of the change this changeset refers to, as "major", "minor", or "patch"
+   *         type
+   *         // This needs to be explained - see @changesets/changelog-github's code for how this works
+   *         changelogOpts
+   *     ) => string
+   */
+  getReleaseLine: async(
+      changeset,
+      type,
+      options,
+  ) => {
     if (!options || !options.repo) {
       throw new Error(
         'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]',
@@ -46,11 +86,12 @@ const changelogFunctions: ChangelogFunctions = {
     let prFromSummary: number | undefined;
     let commitFromSummary: string | undefined;
     const usersFromSummary: string[] = [];
+    const ignoredUsers = ['dependabot'];
 
     const replacedChangelog = changeset.summary
       .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
         const num = Number(pr);
-        if (!isNaN(num)) prFromSummary = num;
+        if (!Number.isNaN(num)) prFromSummary = num;
         return '';
       })
       .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
@@ -58,6 +99,8 @@ const changelogFunctions: ChangelogFunctions = {
         return '';
       })
       .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
+        if (ignoredUsers.find(user)) return '';
+
         usersFromSummary.push(user);
         return '';
       })
@@ -69,18 +112,13 @@ const changelogFunctions: ChangelogFunctions = {
 
     const links = await (async() => {
       if (prFromSummary !== undefined) {
-        let { links } = await getInfoFromPullRequest({
+        const { links } = await getInfoFromPullRequest({
           repo: options.repo,
           pull: prFromSummary,
         });
-        if (commitFromSummary) {
-          links = {
-            ...links,
-            commit: `[\`${commitFromSummary}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
-          };
-        }
         return links;
       }
+
       const commitToFetchFrom = commitFromSummary || changeset.commit;
       if (commitToFetchFrom) {
         const { links } = await getInfo({
@@ -89,6 +127,7 @@ const changelogFunctions: ChangelogFunctions = {
         });
         return links;
       }
+
       return {
         commit: null,
         pull: null,
