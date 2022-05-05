ba0de58af33bf27d1f5c20408ebcdba7132b8bf8
Ryu Sato <ryu@weseek.co.jp>
2022-05-06 04:29:23 +0900

    fix workflow

--- packages/custom-changelog/dist/category.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type Category = {
-    text?: string;
-    changesetSummaryPatterns?: Array<string>;
-    pullRequestLabels?: Array<string>;
-};
-export declare function prefixSummary(summary: string, categories: Array<Category>, owner: string, repo: string, commitID: string): Promise<string>;
