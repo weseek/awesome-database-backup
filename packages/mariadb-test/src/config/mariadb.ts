90220bfbd987b4625be9c537ac122e508abf97c3
Ryu Sato <ryu@weseek.co.jp>
2022-05-21 15:31:54 +0900

    fix env name

--- packages/mariadb-test/src/config/mariadb.ts
+++ packages/mariadb-test/src/config/mariadb.ts
@@ -1,6 +1,6 @@
 export const mariadbConfig = {
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
-  host: process.env.MYSQLL_HOST || 'mariadb',
+  host: process.env.MYSQL_HOST || 'mariadb',
   port: Number(process.env.MYSQL_PORT) || 3306,
 };
