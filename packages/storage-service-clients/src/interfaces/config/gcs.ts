93365c189f5cc0d4c75b0702eff7fda91faf8f7a
Syunsuke Komma <syunsuke@weseek.co.jp>
2024-05-08 06:58:41 +0900

    remove StorageServiceClientConfig interfaces

--- packages/storage-service-clients/src/interfaces/config/gcs.ts
+++
@@ -1,8 +0,0 @@
-export interface GCSStorageServiceClientConfig {
-  gcpEndpointUrl?: URL,
-  gcpServiceAccountKeyJsonPath?: string,
-  gcpProjectId?: string,
-  gcpClientEmail?: string,
-  gcpPrivateKey?: string,
-}
-export default GCSStorageServiceClientConfig;
--- packages/storage-service-clients/src/interfaces/index.ts
+++ packages/storage-service-clients/src/interfaces/index.ts
@@ -10,8 +10,6 @@ export interface IStorageServiceClient {
   copyFile(copySource: string, copyDestination: string): Promise<void>,
 }
 
-export * from './config/gcs';
-export * from './config/s3';
 export * from './method-options/gcs';
 export * from './method-options/s3';
 export * from './uri/gcs';
