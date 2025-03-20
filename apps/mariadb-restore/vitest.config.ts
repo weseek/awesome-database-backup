9e2188c2acf6e06ad9485b4be0169640c0c69f52
Ryu Sato <ryu@weseek.co.jp>
2025-03-20 17:34:43 +0900

    make lcov report

--- apps/mariadb-restore/vitest.config.ts
+++ apps/mariadb-restore/vitest.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       enabled: true,
-      reporter: ['text', 'json', 'html'],
+      reporter: ['text', 'json', 'html', 'lcov'],
       reportsDirectory: './coverage',
     },
     ...(process.env.CI
