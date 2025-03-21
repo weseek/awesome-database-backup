7b9fe49f55a3a80cbad18307bb79ec9552bea4d9
Ryu Sato <ryu@weseek.co.jp>
2025-03-22 01:32:21 +0900

    performance test

--- apps/file-backup/__tests_performance__/backup.test.ts
+++ apps/file-backup/__tests_performance__/backup.test.ts
@@ -12,6 +12,7 @@ import {
   testGCSBucketURI,
   cleanTestGCSBucket,
   listFileNamesInTestGCSBucket,
+  initFakeGCSServer,
 } from '@awesome-database-backup/storage-service-test';
 import {
   getTestDirPath,
@@ -23,49 +24,53 @@ const exec = promisify(execOriginal);
 const execBackupCommand = 'yarn run ts-node src/backup';
 
 describe('backup', () => {
-  describe('when valid S3 options are specified', () => {
-    beforeEach(cleanTestS3Bucket);
-    beforeEach(() => prepareLargeTestFile(1024));
+  describe('when valid GCS options are specified', () => {
+    beforeEach(initFakeGCSServer);
+    beforeEach(cleanTestGCSBucket);
+    beforeEach(() => {
+      prepareLargeTestFile(256);
+    });
 
     describe('and when backup tool options are specified', () => {
       const commandLine = `${execBackupCommand} \
-        --aws-endpoint-url ${s3ClientConfig.endpoint} \
-        --aws-region ${s3ClientConfig.region} \
-        --aws-access-key-id ${s3ClientConfig.credentials.accessKeyId} \
-        --aws-secret-access-key ${s3ClientConfig.credentials.secretAccessKey} \
+        --gcp-endpoint-url ${storageConfig.apiEndpoint} \
+        --gcp-project-id ${storageConfig.projectId} \
+        --gcp-client-email ${storageConfig.credentials.client_email} \
+        --gcp-private-key ${storageConfig.credentials.private_key} \
         --backup-tool-options "-v ${getTestDirPath()}" \
-        --target-bucket-url ${testS3BucketURI} \
-        --save-with-tempfile`;
+        --target-bucket-url ${testGCSBucketURI}/`;
 
       it('backup files in bucket', async() => {
+        expect((await listFileNamesInTestGCSBucket()).length).toBe(0);
         expect(await exec(commandLine)).toEqual({
           stdout: expect.stringMatching(/=== backup.ts started at .* ===/),
           stderr: '',
         });
+        expect((await listFileNamesInTestGCSBucket()).length).toBe(1);
       });
     });
   });
 
-  // describe('when valid S3 options with stream mode are specified', () => {
-  //   beforeEach(cleanTestS3Bucket);
+  // describe('when valid GCS options with stream mode are specified', () => {
+  //   beforeEach(cleanTestGCSBucket);
   //   beforeEach(prepareTestFile);
 
   //   describe('and when backup tool options are specified', () => {
   //     const commandLine = `${execBackupCommand} \
-  //       --aws-endpoint-url ${s3ClientConfig.endpoint} \
-  //       --aws-region ${s3ClientConfig.region} \
-  //       --aws-access-key-id ${s3ClientConfig.credentials.accessKeyId} \
-  //       --aws-secret-access-key ${s3ClientConfig.credentials.secretAccessKey} \
+  //       --gcp-endpoint-url ${storageConfig.apiEndpoint} \
+  //       --gcp-project-id ${storageConfig.projectId} \
+  //       --gcp-client-email ${storageConfig.credentials.client_email} \
+  //       --gcp-private-key ${storageConfig.credentials.private_key} \
   //       --backup-tool-options "-v ${getTestDirPath()}" \
-  //       --target-bucket-url ${testS3BucketURI}`;
+  //       --target-bucket-url ${testGCSBucketURI}/`;
 
   //     it('backup files in bucket using stream mode', async() => {
-  //       expect((await listFileNamesInTestS3Bucket()).length).toBe(0);
+  //       expect((await listFileNamesInTestGCSBucket()).length).toBe(0);
   //       expect(await exec(commandLine)).toEqual({
   //         stdout: expect.stringMatching(/=== backup.ts started at .* \(stream mode\) ===/),
   //         stderr: '',
   //       });
-  //       expect((await listFileNamesInTestS3Bucket()).length).toBe(1);
+  //       expect((await listFileNamesInTestGCSBucket()).length).toBe(1);
   //     });
   //   });
   // });
